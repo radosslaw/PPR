5c3ffc9730e002e5b15e0f9f7f350ce3508eab1d
radosslaw <radoslaw.motyka3@gmail.com>
2021-03-31 17:32:55 +0200

    initial commit

---
+++ Lab1/client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			byte[] bytes = new byte[1024];
+			try {
+				IPHostEntry ipHostInfo = Dns.GetHostEntry(args.Length > 0 ? args[0] : "localhost");
+				IPAddress ipAddress = ipHostInfo.AddressList[0];
+				IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+				Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp );
+				try {
+					sender.Connect(remoteEP);  
+					Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
+					byte[] msg = Encoding.Unicode.GetBytes("Test message");
+					sender.Send(msg);
+					Console.WriteLine($"Sent: {Encoding.Unicode.GetString(msg)}");
+					sender.Receive(bytes);
+					Console.WriteLine($"Received: {Encoding.Unicode.GetString(bytes)}");
+					sender.Shutdown(SocketShutdown.Both);  
+					sender.Close();
+				} catch (Exception e) {
+					Console.WriteLine($"Unexpected exception : {e.ToString()}");
+				}
+			} catch (Exception e) {
+				Console.WriteLine( e.ToString());
+			}
+        }
+    }
+}
