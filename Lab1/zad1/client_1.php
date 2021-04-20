fd5222ac0bfa03dfea5f81d3218980ec6720a354
radosslaw <radoslaw.motyka@student.wat.edu.pl>
2021-04-20 15:35:17 +0200

    zad1 gotowe

---
+++ Lab1/zad1/client_1.php
@@ -0,0 +1,35 @@
+#!/usr/bin/php
+
+<?php
+	# zmienne predefiniowane -------------------------------------------
+	$host = "127.0.0.1";
+	$port = 12345;
+	
+	# tworzymy gniazdo -------------------------------------------------
+	if( ! ( $client = socket_create( AF_INET, SOCK_STREAM, SOL_TCP ) ) ){
+		print "socket_create(): " 	. socket_strerror( socket_last_error( $client ) ) . "\n";
+		exit( 1 );
+	}
+	
+	# podlaczamy gniazdo do serwera ------------------------------------
+	if( ! socket_connect( $client, $host, $port ) ){
+		print "socket_connect(): " 	. socket_strerror( socket_last_error( $client ) ) . "\n";
+		exit( 1 );
+	}
+		
+	# odczytujemy wiadomosc od serwera ---------------------------------
+	echo "Podaj wiadomosc: ";
+	$msg = rtrim(fgets(STDIN), "\n"); 
+
+	if (!socket_send($client, $msg, strlen($msg), 0)) { 
+		echo "Error! Sending not completed\n";  
+	}
+	
+	# zamykamy gniazdo -------------------------------------------------
+	socket_close( $client );
+	
+	# wyswietlamy wiadomosc --------------------------------------------
+	echo "Twoja wiadomosc: ";
+	print "$msg\n";
+	#-------------------------------------------------------------------
+?>
